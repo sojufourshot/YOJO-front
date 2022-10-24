b332b27981c0d08211682f07d6bed842a4821b6d
unknown <selexted>
2022-10-24 15:44:54 +0900

    front/feat:명기의 캠 페이지

---
+++ src/pages/cam/Cam.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useState, Component } from "react";
+import "../../style/Cam.css"
+
+
+
+
+const Cam = () => {
+
+
+	return (
+		<div className="cam_background">
+			a
+		</div>
+	);
+};
+
+export default Cam;
