2c2412de7c8bddb65507e0aeb11cc3a65ae2e5cd
riroan <riroan@naver.com>
2022-10-24 17:22:36 +0900

    front/feat: 카메라 페이지 추가

--- src/pages/cam/Cam.jsx
+++ src/pages/cam/Cam.jsx
@@ -1,17 +1,45 @@
-import React, { useEffect, useState, Component } from "react";
-import "../../style/Cam.css"
-
-
-
+import React, { useState } from 'react'
+import Button from '@mui/material/Button'
+import '../../style/Cam.css'
+import Camera from '../../components/layout/Camera'
 
 const Cam = () => {
-
-
+	const title = '자세 1'
+	const [img, setImg] = useState('')
+	const send = ()=>{
+		console.log("send")
+	}
 	return (
-		<div className="cam_background">
-			a
+		<div className="background">
+			<div className="title">{title}</div>
+			<hr />
+			{img === '' ? (
+				<Camera func={image => setImg(image)} />
+			) : (
+				<div>
+					<div className="image">
+						<img src={img} alt="alt" />
+					</div>
+					<div className="msg">전송하시겠습니까?</div>
+					<div className="buttonSet">
+						<Button
+							onClick={() => {
+								setImg('')
+							}}
+							sx={{ m: 0.5 }}
+							className="button"
+							variant="outlined"
+						>
+							취소
+						</Button>
+						<Button onClick={()=>{send()}} sx={{ m: 0.5 }} className="button" variant="contained">
+							전송
+						</Button>
+					</div>
+				</div>
+			)}
 		</div>
-	);
-};
+	)
+}
 
-export default Cam;
+export default Cam
