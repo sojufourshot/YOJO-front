4e54d7a925795239284b4b35571c6f58f13accd6
kjchoi1997 <kjchoi199780@gmail.com>
2022-11-06 18:03:22 +0900

    front/feat: 업로드 디폴트 이미지

--- src/pages/evaluate/UploadPage.jsx
+++ src/pages/evaluate/UploadPage.jsx
@@ -3,12 +3,13 @@ import { useEffect, useState } from "react";
 import "../../style/uploadpage.css";
 import { Button } from "@mui/material";
 import { Link } from "react-router-dom";
+import x from "../../assets/images/x.png";
 import axios from "axios";
 
 const UploadPage = () => {
 	const [image, setImage] = useState({
 		image_file: "",
-		preview_URL: "../../assets/images/soju.jpeg",
+		preview_URL: x,
 	});
 
 	const msg = {
@@ -35,7 +36,7 @@ const UploadPage = () => {
 		URL.revokeObjectURL(image.preview_URL);
 		setImage({
 			image_file: "",
-			preview_URL: "img/default_image.png",
+			preview_URL: x,
 		});
 	};
 
@@ -54,17 +55,18 @@ const UploadPage = () => {
 			formData.append("poseName", "활자세");
 			console.log(formData); // FormData {}
 			for (const keyValue of formData) console.log(keyValue); // ["img", File] File은 객체
-			await axios.post("http://localhost:5500/api/v1/images", formData, { withCredentials: true })
-			.then((res) => {
-				console.log(res)
-			})
-			.catch((err) => {
-				console.log(err)
-			});
+			await axios
+				.post("http://localhost:5500/api/v1/images", formData, { withCredentials: true })
+				.then((res) => {
+					console.log(res);
+				})
+				.catch((err) => {
+					console.log(err);
+				});
 			alert("서버에 등록이 완료되었습니다!");
 			setImage({
 				image_file: "",
-				preview_URL: "img/default_image.png",
+				preview_URL: x,
 			});
 		} else {
 			alert("사진을 등록하세요!");
